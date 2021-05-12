57e27f8d6ce963709a2e10edae3b6408ec45f5b3
Tea <teasalonsaari99@gmail.com>
2021-05-12 14:46:31 +0300

    workin gon anekdootit

--- osa1/1.12. - 1.14. anekdootit/src/App.js
+++ osa1/1.12. - 1.14. anekdootit/src/App.js
@@ -8,28 +8,33 @@ const Button = (props) => {
   )
 }
 
+const VoteCount = (props) => {
+  return (
+    <div>has {props.votecount} {console.log(props.votecount)}votes</div>
+  )
+}
+
 const App = () => {
   const anecdotes = [
-    'If it hurts, do it more often',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+    '0If it hurts, do it more often',
+    '1Adding manpower to a late software project makes it later!',
+    '2The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    '3Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    '4Premature optimization is the root of all evil.',
+    '5Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
   ]
-
-  let votes = Array.apply(null, new Array(6)).map(Number.prototype.valueOf,0);
-  const copy = [...votes]
    
   const [selected, setSelected] = useState(0)
+  const votes = Array.apply(null, new Array(6)).map(Number.prototype.valueOf,0)
+  let [copy, setCopy] = useState(votes)
 
   const handleVoteClick = () => {
-    console.log('voting now ' + selected)
-    copy[selected] += 1
+    console.log(copy)
+    //setCopy(copy[selected] += 1);
   }
 
   const handleNextClick = () => {
-    setSelected(Math.floor(Math.random() * 6))
+    setSelected(Math.floor(Math.random() * (6 - 0) + 0))
   }
 
   return (
@@ -37,6 +42,7 @@ const App = () => {
       <div>
         {anecdotes[selected]}
       </div>
+      <VoteCount votecount={copy[selected]}/>
       <div>
         <Button handleClick={handleVoteClick} text={'vote'}/>
         <Button handleClick={handleNextClick} text={'next anecdote'}/>
