3be1b8907a372000fa69ae04c535b16d89eb0cf6
Tea Salonsaari <salotea@digitalentshelsinki.internal>
2021-08-27 12:03:42 +0200

    began working on maidentiedot

--- osa2/3maidentiedot/src/App.js
+++ osa2/3maidentiedot/src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+const Filter = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    find countries <input 
+    value={props.filter}
+    onChange={props.handleFilter}/>
     </div>
-  );
+  )
+}
+
+const Content = (props) => {
+  const content = null
+  {props.countries.lenght} > 10 ?
+  content = <p>Too many matches, specify another filter</p>
+  :
+  {props.countries.lenght} <= 10 && {props.countries.lenght} > 1?
+  content = props.countries
+  :
+  {props.countries.lenght} === 1 ?
+  content =
+  <div>
+    <h1>{props.countries.name}</h1>
+    <p>capital {props.countries.capital}</p>
+    <p>population {props.countries.population}</p>
+    <h2>languages</h2>
+    <ul>
+      {props.countries.map(country => <li>{country.language}</li> )}
+    </ul>
+  </div>
+  :
+  console.log("no input")
+  return (
+    <div>
+      {content}
+    </div>
+  )
+}
+
+
+const App = () => {
+   const [countries, setCountries] = useState([])
+   const [filter, setFilter] = useState('')
+   const [filteredCountries, setFilteredCountries] = useState([])
+
+  useEffect(() => {
+    countryService
+      .getAll()
+        .then(initialCountries => {
+        setCountries(initialCountries)
+        console.log(countries)
+      })
+  }, [])
+
+  const handleFilter = (event) => {
+    setFilter(event.target.value)
+
+    countryService
+    .getSome(filter)
+    .then(returnedCountries => {
+      setFilteredCountries(returnedCountries)
+    })
+      
+    
+  }
+
+  return(
+    <div>
+      <Filter filter={filter} handleFilter={handleFilter}/>
+      <Content countries={filteredCountries}/>
+    </div>
+  )
 }
 
 export default App;
